c7284a2234b97155a3c18b9ed6cf5e3e6c60cb9d
Talant <nandosorbello@live.it>
2022-11-17 20:01:55 +0100

    Contact Form OK. Need API

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import CenteredContainer from "@/Layouts/CenteredContainer"
 import Footer from "@/UI/Footer"
+import Logo from "@Assets/Logo_Black.png"
+
 
 export default function Contact() {
 	return (
@@ -8,27 +10,100 @@ export default function Contact() {
 			<section id="Contact_Form" className="h-screen flex flex-col justify-end">
 				<CenteredContainer>
 					<div className="mx-auto w-full max-w-lg">
-						<h1 className="text-4xl font-medium">Contact us</h1>
-						<p className="mt-3">Email us at help@domain.com or message us here:</p>
+
+						<div className="w-full h-auto flex justify-center items-center mb-3">
+							<img src={Logo} className="h-20" alt="MoneyKeeper Logo" />
+						</div>
+
+						<h1 className="text-4xl font-medium">Contact Us</h1>
+						<p className="mt-3">Your Opinion is Important to Us.</p>
 
 						<form action="sendToMyApi" className="mt-10">
 					
 							<div className="grid gap-6 sm:grid-cols-2">
 								<div className="relative z-0">
-									{/*Le label dovrebbero essere allineate invece per qualche motivo sono completamente a fanculo*/}
 									<input 
 										type="text" 
 										name="name" 
-										className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" " />
-									<label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Your name</label>
+										className="
+											peer block w-full appearance-none 
+											border-0 border-b border-gray-500
+											bg-transparent 
+											py-2.5 px-0 
+											text-sm 
+											text-gray-900 
+											focus:outline-none 
+											focus:ring-0" placeholder=" " />
+									{/* The Peer Class is used to modify sibilings element. In this case, i put peer on input, and on input change state i change class on label*/}
+									<label className="
+										text-[#9CA3AF]
+										absolute top-0 left-0 -z-10 origin-[0]
+										transform text-lg duration-300 
+										-translate-y-6 scale-75
+
+										peer-placeholder-shown:scale-100
+										peer-placeholder-shown:translate-y-0 
+
+										peer-focus:text-pureblack
+										peer-focus:left-0
+										peer-focus:-translate-y-6
+										peer-focus:scale-75
+									">Your Name</label>
 								</div>
 								<div className="relative z-0">
-									<input type="text" name="email" className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" " />
-									<label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Your email</label>
+									<input type="text" name="email" className="
+											peer block w-full appearance-none 
+											border-0 border-b border-gray-500
+											bg-transparent 
+											py-2.5 px-0 
+											text-sm 
+											text-gray-900 
+											focus:outline-none 
+											focus:ring-0" placeholder=" "/>
+									<label className="
+										text-[#9CA3AF]
+										absolute top-0 left-0 -z-10 origin-[0]
+										transform text-lg duration-300 
+										-translate-y-6 scale-75
+
+										peer-placeholder-shown:scale-100
+										peer-placeholder-shown:translate-y-0 
+
+										peer-focus:text-pureblack
+										peer-focus:left-0
+										peer-focus:-translate-y-6
+										peer-focus:scale-75
+									">Your Email</label>
 								</div>
-								<div className="relative z-0 col-span-2">
-									<textarea name="message" rows={5} className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder="" style={{resize:"none"}}></textarea>
-									<label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Your message</label>
+								<div className="relative z-0 col-span-2 m-1">
+									<textarea 
+										name="message" 
+										rows={5} 
+										className="
+											peer block w-full appearance-none 
+											border-0 border-b border-gray-500 
+											bg-transparent 
+											py-2.5 px-0 
+											text-sm text-gray-900 
+											
+											focus:outline-none 
+											focus:ring-0" 
+										placeholder=" " 
+										style={{resize:"none"}}></textarea>
+									<label className="
+										text-[#9CA3AF]
+										absolute top-0 left-0 -z-10 origin-[0]
+										transform text-lg duration-300 
+										-translate-y-6 scale-75
+
+										peer-placeholder-shown:scale-100
+										peer-placeholder-shown:translate-y-0 
+
+										peer-focus:text-pureblack
+										peer-focus:left-0
+										peer-focus:-translate-y-6
+										peer-focus:scale-75
+									">Your Message</label>
 								</div>
 							</div>
 							<button type="submit" className="mt-5 rounded-md bg-black px-10 py-2 text-white">Send Message</button>
--- src/UI/Footer.tsx
+++ src/UI/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
 			<footer className="p-4 bg-black shadow md:px-6 md:py-8">
 				<div className="sm:flex sm:items-center sm:justify-between">
 					<a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0">
-						<img src={Logo} className="mr-3 h-8" alt="Flowbite Logo" />
+						<img src={Logo} className="mr-3 h-8" alt="MoneyKeeper Logo" />
 						<span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Money Keeper</span>
 					</a>
 					<ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
