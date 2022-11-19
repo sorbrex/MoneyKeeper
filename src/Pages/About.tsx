aab656d54151dfbc0ec33953388230e77f8b0a75
Talant <nandosorbello@live.it>
2022-11-19 03:24:26 +0100

    ref before

---
+++ src/Pages/About.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import CenteredContainer from "@/Layouts/CenteredContainer"
+
+export default function About() {
+	return (
+		<CenteredContainer>
+			<h1>AboutUs</h1>
+		</CenteredContainer>
+	)
+}
