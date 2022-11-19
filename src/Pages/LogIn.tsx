f9533b14ccb0986d81e626b138b39794ef71f532
Talant <nandosorbello@live.it>
2022-11-19 22:48:41 +0100

    I think that i want to make a global form but my brain is broken so lets see

--- src/Pages/LogIn.tsx
+++ src/Pages/LogIn.tsx
@@ -4,7 +4,9 @@ import CenteredContainer from "@/Layouts/CenteredContainer"
 export default function LoginPage() {
 	return (
 		<CenteredContainer>
-			<h1>Login</h1>
+			<div id="Frame" className="border-2 border-pureblack mx-auto p-20">
+				<h3 className="text-2xl font-bold">Log In</h3>
+			</div>
 		</CenteredContainer>
 	)
 }
--- src/Pages/Redirect.tsx
+++ src/Pages/Redirect.tsx
@@ -4,7 +4,9 @@ import CenteredContainer from "@/Layouts/CenteredContainer"
 export default function Redirect() {
 	return (
 		<CenteredContainer>
-			<h1>Redirect</h1>
+			<div id="Frame" className="bg-pageGray w-full h-full">
+				<h3 className="text-2xl font-bold">Redirect</h3>
+			</div>
 		</CenteredContainer>
 	)
 }
