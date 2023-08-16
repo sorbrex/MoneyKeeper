f5592a5681f8a20c10a263c420042786a16f033f
Talant <nandosorbello@live.it>
2023-08-16 11:12:12 +0200

    use real url for production

--- src/Helpers/Helpers.ts
+++ src/Helpers/Helpers.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
-export const BASE_URL = "http://localhost:8080"
+export const BASE_URL = "https://money-keeper-api.onrender.com/" //"http://localhost:8080"
 
 export async function Auth() {
 	const savedJwt = sessionStorage.getItem("users-jwt")
