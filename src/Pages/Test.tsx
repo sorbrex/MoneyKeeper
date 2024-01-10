854976f864fe7665b55cf2fe7bc3c92ada759805
Talant <nandosorbello@live.it>
2024-01-10 17:41:01 +0100

    Working on date picker and calendar logic (show/hide) and full component with button ecc...

--- src/Pages/Test.tsx
+++ src/Pages/Test.tsx
@@ -3,6 +3,8 @@ import CenteredContainer from "@/Layouts/CenteredContainer"
 import DatePicker from "@UI/Complex/DatePicker"
 
 export default function Test() {
+	const [show, setShow] = React.useState(false)
+
 	return (
 		<>
 			<section id="Test_Page" className="h-screen flex flex-col">
@@ -16,8 +18,9 @@ export default function Test() {
 					{/*	<CategoryIcon icon="Hobby"/>*/}
 					{/*	<CategoryIcon icon="Home"/>*/}
 					{/*</div>*/}
-					<DatePicker/>
+					<DatePicker visibile={show} toggleVisibility={setShow} />
 
+					<button className={"px-4 py-2 rounded-md bg-gray-200 text-gray-800 hover:bg-gray-300 hover:text-gray-900 transition-colors duration-300 ease-in-out"} onClick={()=>setShow(true)}>Show</button>
 				</CenteredContainer>
 
 			</section>
--- src/UI/Complex/DatePicker.tsx
+++ src/UI/Complex/DatePicker.tsx
@@ -1,24 +1,44 @@
-import React, {useState} from "react"
-import { DayPicker } from "react-day-picker"
+import React, { useState } from "react"
+import { format, subMonths} from "date-fns"
+import { DateRange, DayPicker} from "react-day-picker"
 
-export default function DatePicker() {
-	const initialDays: Date[] = []
-	const [days, setDays] = useState<Date[] | undefined>(initialDays)
+const pastMonth = subMonths(new Date(), 1)
 
-	const footer =
-		days && days.length > 0 ? (
-			<p>You selected {days.length} day(s).</p>
-		) : (
-			<p>Please pick one or more days.</p>
-		)
+export default function DatePicker(props: {visibile: boolean, toggleVisibility: (state: boolean) => void}){
+	const defaultSelected: DateRange = {
+		from: pastMonth,
+		to: new Date()
+	}
+
+	const [range, setRange] = useState<DateRange | undefined>(defaultSelected)
+
+	function onPick(selectedRange: DateRange | undefined){
+		setRange(selectedRange)
+		if (selectedRange === undefined || selectedRange.from === undefined || selectedRange.to === undefined) return
+		console.log(selectedRange)
+		const assertedRange = selectedRange as DateRange
+		const timestampFrom = format(assertedRange.from as Date , "T")
+		const timestampTo = format(assertedRange.to as Date , "T")
+		console.log("From => " + timestampFrom)
+		console.log("To => " + timestampTo)
+	}
+
 
 	return (
-		<DayPicker
-			mode="multiple"
-			min={1}
-			selected={days}
-			onSelect={setDays}
-			footer={footer}
-		/>
+		<div className="relative border-2 border-solid border-black m-2">
+			<div id="Calendar" className={`relative ${props.visibile ? "" : "hidden"}`}>
+				<DayPicker
+					mode="range"
+					defaultMonth={pastMonth}
+					selected={range}
+					onSelect={onPick}
+					footer={
+						<div className="flex flex-row justify-center">
+							<button className="px-4 py-2 rounded-md bg-gray-200 text-gray-800 hover:bg-gray-300 hover:text-gray-900 transition-colors duration-300 ease-in-out" onClick={() => props.toggleVisibility(false)}>Save</button>
+						</div>
+					}
+				/>
+			</div>
+		</div>
 	)
 }
