a1ff09c95500649d52561b246694c3697db5b886
Sorbrex <nsorbello@movia.biz>
2024-02-01 12:39:34 +0100

    Fixed Transaction Chart (Bar) in the Dashboard

--- src/Pages/App/Dashboard/Components/ClusteredChart.tsx
+++ src/Pages/App/Dashboard/Components/ClusteredChart.tsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from "react"
-import {Category, Transaction} from "@/Types/Types"
-import {getAllColors} from "@UI/Simple/CategoryIcon"
+import {Transaction} from "@/Types/Types"
 import {
 	Chart as ChartJS,
 	CategoryScale,
@@ -11,8 +10,7 @@ import {
 	Legend,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
-import dayjs from "dayjs";
-
+import {getAllColors} from "@UI/Simple/CategoryIcon";
 ChartJS.register(
 	CategoryScale,
 	LinearScale,
@@ -28,14 +26,6 @@ export default function ClusteredChart(props: ClusteredChartProps) {
 
 	const options = {
 		responsive: true,
-		scales: {
-			x: {
-				stacked: true,
-			},
-			y: {
-				stacked: true,
-			},
-		},
 		plugins: {
 			legend: {
 				display: false
@@ -47,79 +37,68 @@ export default function ClusteredChart(props: ClusteredChartProps) {
 		datasets: [
 			{
 				label: "Amount",
-				data: [0]
+				data: [0],
+				backgroundColor: getAllColors()[0]
 			}
 		]
 	})
 
-	function updateChartData (data: Transaction[], categoryList: Category[]) {
-		if (!data || data.length <= 0) {
+	function updateData(originalData: Transaction[]) {
+		if (!originalData || originalData.length === 0) {
 			return setChartData({
 				...chartData,
-				labels: ["None"],
+				labels: [""],
 				datasets: [
 					{
-						...chartData.datasets[0],
-						label: "Amount",
-						data: [0]
+						label: "None",
+						data: [0],
+						backgroundColor: getAllColors()[0],
 					}
-				]
+				],
 			})
 		}
 
-		const oldLabels = data.map((transaction: Transaction) => dayjs(transaction.createdAt).format("DD/MM/YYYY"))
+		const parsedData: {[key: string]: {[key:string]: number}} = {};
 
-		console.log("Old Labels: ", oldLabels)
+		originalData.forEach((singleTransaction) => {
+			const date = new Date(singleTransaction.createdAt).toLocaleDateString();
 
-		const labels = new Set<string>()
-		oldLabels.forEach((label) => labels.add(label))
-
-		// Initialize the final object with arrays of zeros for each type of transaction
-		const resultObject: {[key: string]: number[]} = {}
-
-		resultObject['expense'] = Array(labels.size).fill(0)
-		resultObject['income'] = Array(labels.size).fill(0)
-
-		// Loop through the original array and update the result object
-		data.forEach((transaction, index) => {
-			resultObject[transaction.type][index] += transaction.amount || 0
-		})
-
-		console.log("Result Object: ", resultObject)
-
-		const dataset = Object.keys(resultObject).map((type) => {
-			return {
-				fill: true,
-				label: type,
-				data: resultObject[type],
-				backgroundColor: type === "expense" ? "#F27289" : "#00B2A3",
-				stack: type
+			if (!parsedData[date]) {
+				parsedData[date] = { expense: 0, income: 0 };
 			}
-		})
+
+			if (singleTransaction.type === 'income') {
+				parsedData[date].income += singleTransaction.amount;
+			} else if (singleTransaction.type === 'expense') {
+				parsedData[date].expense += singleTransaction.amount;
+			}
+		});
+
+		const uniqueDates = Object.keys(parsedData)
+
+		const datasets = [
+			{
+				label: 'Income',
+				data: uniqueDates.map((date) => parsedData[date]?.income || 0),
+				backgroundColor: "#00B2A3", // Green color for income
+			},
+			{
+				label: 'Expense',
+				data: uniqueDates.map((date) => parsedData[date]?.expense || 0),
+				backgroundColor: "#F27289", // Red color for expenses
+			},
+		];
+
 
 		setChartData({
 			...chartData,
-			labels: Array.from(labels),
-			datasets: dataset,
+			labels: uniqueDates,
+			datasets: datasets
 		})
 	}
 
-
 	useEffect(() => {
-		updateChartData(props.data, props.categoryList)
-		// console.log("Chart Data: ", chartData)
-		// const data = props.data.map((category: CategoryWithAmount) => category.amount)
-		//
-		// setChartData({
-		// 	...chartData,
-		// 	labels: labels,
-		// 	datasets: [
-		// 		{
-		// 			...chartData.datasets[0],
-		// 			data: data
-		// 		}
-		// 	]
-		// })
+		updateData(props.data)
 	}, [props])
 
 	return(
@@ -129,5 +108,4 @@ export default function ClusteredChart(props: ClusteredChartProps) {
 
 type ClusteredChartProps = {
 	data: Transaction[]
-	categoryList: Category[]
 }
