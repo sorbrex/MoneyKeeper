d56b1ad1be3d229050b88cbb375f1724e40289aa
Sorbrex <nsorbello@movia.biz>
2024-02-01 11:50:34 +0100

    Fixed Transactions Chart (Area)

--- src/Pages/App/Transaction/Components/TransactionChart.tsx
+++ src/Pages/App/Transaction/Components/TransactionChart.tsx
@@ -19,7 +19,6 @@ import {
 } from "chart.js"
 import {getAllColors} from "@UI/Simple/CategoryIcon"
 import {_DeepPartialObject} from "chart.js/dist/types/utils"
-import dayjs from "dayjs"
 import "chart.js/auto" // ADD THIS
 
 ChartJS.register(
@@ -33,69 +32,11 @@ ChartJS.register(
 	Legend
 )
 
-function parseDataForAreaChart(transactions: Transaction[], categoryList: Category[]) {
-	const chartData = {};
-	if (!transactions || transactions.length === 0) return
-	transactions.forEach((transaction) => {
-		const date = new Date(transaction.createdAt).toLocaleDateString();
-		const category = transaction.categoryId;
-
-		if (!chartData[date]) {
-			chartData[date] = {};
-		}
-
-		if (!chartData[date][category]) {
-			chartData[date][category] = 0;
-		}
-
-		if (transaction.type === 'expense') {
-			chartData[date][category] += transaction.amount;
-		}
-	});
-
-	const uniqueDates = Array.from(new Set(transactions.map((t) => new Date(t.createdAt).toLocaleDateString())));
-	const keys = Object.keys(chartData);
-	console.log("Keys: ", keys)
-	console.log("chartData: ", chartData)
-
-	keys.map((singleDate) => console.log("Single Date Object: ", chartData[singleDate]))
-
-	// const datasets = Object.keys(chartData).map((date) => ({
-	// 	label: category,
-	// 	data: chartData[date].map((category) => chartData[date][category]),
-	// 	fill: true,
-	// 	borderColor: getRandomColor(),
-	// 	backgroundColor: 'rgba(75,192,192,0.2)', // You can customize the background color
-	// }));
-
-	//
-	// console.log("parseDataForAreaChart: ", {
-	// 	labels: uniqueDates,
-	// 	datasets,
-	// })
-
-	// return {
-	// 	labels: uniqueDates,
-	// 	datasets,
-	// };
-}
-
-function getRandomColor() {
-	const letters = '0123456789ABCDEF';
-	let color = '#';
-	for (let i = 0; i < 6; i++) {
-		color += letters[Math.floor(Math.random() * 16)];
-	}
-	return color;
-}
-
-
-
 export default function TransactionAreaChart(props: TransactionAreaChartProps) {
 
 	// Normalize Data For Chart
-	function updateData (data: Array<Transaction>, categoryList: Array<Category>, showIncome = false) {
-		if (!data || data.length === 0) {
+	function updateData (originalData: Array<Transaction>, categoryList: Array<Category>, showIncome = false) {
+		if (!originalData || originalData.length === 0) {
 			setChartData({
 				...chartData,
 				labels: [""],
@@ -112,46 +53,66 @@ export default function TransactionAreaChart(props: TransactionAreaChartProps) {
 			return console.log("No Data")
 		}
 
+		let filteredData: Transaction[] = []
+
 		//Expense or Income?
 		if(showIncome) {
-			data = data.filter((transaction: Transaction) => transaction.type === "income")
+			filteredData = originalData.filter((transaction: Transaction) => transaction.type === "income")
 		} else {
-			data = data.filter((transaction: Transaction) => transaction.type === "expense")
+			filteredData = originalData.filter((transaction: Transaction) => transaction.type === "expense")
 		}
 
-		// Initialize the final object with arrays of zeros
-		const resultObject: {[key: string]: number[]} = {}
-		categoryList.forEach(category => {
-			resultObject[category.name] = Array(data.length).fill(0)
-		})
+		if (!filteredData || filteredData.length === 0) return
 
-		// Loop through the original array and update the result object
-		data.forEach((transaction, index) => {
-			const categoryName = categoryList.find(category => category.id === transaction.categoryId)?.name || "Other"
-			resultObject[categoryName][index] = transaction.amount || 0
-		})
+		const parsedData: {[key: string]:{[key: string]:number}} = {};
 
-		const labels = data.map((transaction: Transaction) => dayjs(transaction.createdAt).format("DD/MM/YYYY"))
-		const dataset = Object.keys(resultObject).map((categoryName) => {
-			return {
-				fill: true,
-				label: categoryName,
-				data: resultObject[categoryName],
-				backgroundColor: "",
-				borderColor: "",
+		filteredData.reverse().forEach((singleTransaction) => {
+			const date = new Date(singleTransaction.createdAt).toLocaleDateString();
+			const categoryId = singleTransaction.categoryId;
+
+			if (!parsedData[date]) {
+				parsedData[date] = {};
 			}
-		})
 
-		for (let i = 0; i < dataset.length; i++) {
-			const element = dataset[i]
-			element.borderColor = getAllColors()[i]
+			if (!parsedData[date][categoryId]) {
+				parsedData[date][categoryId] = 0;
+			}
+
+			if (singleTransaction.amount) {
+				parsedData[date][categoryId] += singleTransaction.amount;
+			}
+		});
+
+		const uniqueDates = Object.keys(parsedData)
+
+		// Ensure each category has a value for every date
+		categoryList.forEach((categoryObject) => {
+			uniqueDates.forEach((date) => {
+				if (!parsedData[date][categoryObject.id]) {
+					parsedData[date][categoryObject.id] = 0;
+				}
+			});
+		});
+
+
+		const datasets = categoryList.map((categoryObject) => ({
+			label: categoryObject.name,
+			data: uniqueDates.map((date) => parsedData[date]?.[categoryObject.id] || 0),
+			fill: true,
+			backgroundColor: "",
+			borderColor: "",
+		}));
+
+		for (let i = 0; i < datasets.length; i++) {
+			const element = datasets[i]
 			element.backgroundColor = getAllColors()[i] + "50"
+			element.borderColor = getAllColors()[i]
 		}
 
 		setChartData({
 			...chartData,
-			labels: labels,
-			datasets: dataset,
+			labels: uniqueDates,
+			datasets: datasets,
 		})
 	}
 
@@ -196,13 +157,7 @@ export default function TransactionAreaChart(props: TransactionAreaChartProps) {
 
 
 	useEffect(() => {
-		//updateData(props.data, props.categoryList, props.showIncome)
-		const chartData = parseDataForAreaChart(props.data, props.categoryList)
-		// setChartData({
-		// 	...chartData,
-		// 	labels: chartData.labels,
-		// 	datasets: chartData.datasets,
-		// })
+		updateData(props.data, props.categoryList, props.showIncome)
 	}, [props])
 
 	return (
